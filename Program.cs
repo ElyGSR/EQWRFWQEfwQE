52216e13913f0f060b2b5c94f156efdd2e8ae2fe
ElyGSR <elygabrielsanchezrossell@gmail.com>
2026-09-23 12:37:17 -0500

    impresiondenotas

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Funcion de imrpesion de notas
+static void ImprimirNotas(int[]notasx)
+{
+    for (int i=0;i<notasx.Length;i++)
+    {
+        Console.WriteLine($"Nota {i+1}: {notasx[i]}");
+    }
+}
+int[] notas = {15,18,12,17,20};
+
+//imprimir el arreglo de notas
+ImprimirNotas(notas);
