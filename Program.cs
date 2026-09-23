041cfae7c008051db9c9092fdf1f9e4dddff717b
ElyGSR <elygabrielsanchezrossell@gmail.com>
2026-09-23 12:57:44 -0500

    Promedio de notas Usando Funcion

--- Program.cs
+++ Program.cs
@@ -6,17 +6,23 @@ static void ImprimirNotas(int[]notasx)
         Console.WriteLine($"Nota {i+1}: {notasx[i]}");
     }
 }
+//Funcion de promedio de notas
+static double PromedioNotas(int[] notasx)
+{
+    int suma = 0;
+    for (int i=0;i<notasx.Length;i++)
+    {
+        suma += notasx[i];
+    }
+    return (double)suma /notasx.Length;
+}
 int[] notas = {15,18,12,17,20};
 
 //imprimir el arreglo de notas
 ImprimirNotas(notas);
 
 //promedio de las notas
-int suma = 0;
-for (int i=0;i<notas.Length;i++)
-{
-    suma += notas[i];
-}
-Console.WriteLine($"Promedio de las notas: {(double)suma / notas.Length:F2}");
+double promedio = PromedioNotas(notas);
+Console.WriteLine($"Promedio de las notas: {promedio:f2}");
 
 
